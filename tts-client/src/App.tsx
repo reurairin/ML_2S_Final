1e639332537c0641553a17ee7caa112509a10410
Danil Makushev <reurairin@gmail.com>
2023-04-15 00:11:57 +0300

    basic form functionality

--- tts-client/src/App.tsx
+++ tts-client/src/App.tsx
@@ -1,11 +1,13 @@
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { AppBar } from './components/AppBar/AppBar';
 import { TTSPage } from './components/TTSPage/TTSPage';
 
 export function App() {
+    const queryClient = new QueryClient();
     return (
-        <>
+        <QueryClientProvider client={queryClient}>
             <AppBar />
             <TTSPage />
-        </>
+        </QueryClientProvider>
     );
 }
--- tts-client/src/components/InputForm/InputForm.tsx
+++ tts-client/src/components/InputForm/InputForm.tsx
@@ -1,3 +1,29 @@
+import { useForm } from 'react-hook-form';
+
+const defaultFormState = {
+    userInput: ''
+};
+
 export function InputForm() {
-    return <p>InputForm works</p>;
+    const { register, handleSubmit, reset } = useForm<{ userInput: string }>({
+        defaultValues: defaultFormState
+    });
+
+    const submitTTS = (data: { userInput: string }) => {
+        console.log(data);
+        reset(defaultFormState);
+    };
+
+    return (
+        <form onSubmit={handleSubmit(submitTTS)}>
+            <label htmlFor="userInput">
+                <input
+                    type="text"
+                    {...register('userInput', { required: true })}
+                ></input>
+            </label>
+
+            <button type="submit">To Speech</button>
+        </form>
+    );
 }
