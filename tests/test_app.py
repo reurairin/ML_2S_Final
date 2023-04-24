0256a935f68e2f5e524f5d6ac911fe803542d967
Evgeniya Prasolova <prasolova_evgesha@mail.ru>
2023-04-24 22:47:58 +0300

    add generate test

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,3 +2,7 @@ from fastapi.testclient import TestClient
 from app import app
 
 client = TestClient(app)
+
+def test_generate():
+    response = client.get('/api/generate?utterance=hello')
+    assert response.status_code == 200
