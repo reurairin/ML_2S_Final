3b6cae02936d64c82dc5fb0ae6804d246ef6f32a
Evgeniya Prasolova <prasolova_evgesha@mail.ru>
2023-04-25 21:15:14 +0300

    fix tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,5 @@
 from fastapi.testclient import TestClient
+from io import BytesIO
 from app import app
 
 client = TestClient(app)
@@ -6,3 +7,5 @@ client = TestClient(app)
 def test_generate():
     response = client.get('/api/generate?utterance=hello')
     assert response.status_code == 200
+    assert response.headers["Content-Type"] == "audio/wav"
+    assert isinstance(BytesIO(response.content).read(), bytes)
